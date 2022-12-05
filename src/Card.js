a08ae7cc0e491e02e2edd4c4e560adb7d3eb9c2b
pinkjagnie <olga.krzysztofik@gmail.com>
2022-12-05 21:29:01 +0100

    Added text & button with styles

--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { AiOutlineArrowRight } from "react-icons/ai";
+
 import desk from "./img/desk.jpg";
 
 import styles from "./card.module.css";
@@ -11,6 +13,11 @@ const Card = () => {
         <div className={styles.imgBox}>
           <img src={desk} alt="desk with laptop and notebook" />
         </div>
+        <div className={styles.textBox}>
+          <h1>Noteworthy technology acquisitions 2021</h1>
+          <p>Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        </div>
+        <button>Read more <AiOutlineArrowRight className={styles.arrow}/></button>
       </div>
     </div>
   )
